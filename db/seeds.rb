505181f5e6880a1ab2519f370dbc4865ceaa1e3d
Joshua <joycejosh7@gmail.com>
2021-02-04 13:58:34 -0500

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TvShow.create(name: "Scrubs")
+TvShow.create(name: "30 Rock")
+TvShow.create(name: "Superstore")
+TvShow.create(name: "Spider-man")
