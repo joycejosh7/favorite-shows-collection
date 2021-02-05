2198fbcbdc948bed9cbf73d380a1e47e03cac5d3
Joshua <joycejosh7@gmail.com>
2021-02-05 13:23:38 -0500

    added new form and styled it

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -4,6 +4,16 @@ class TvShowsController < ApplicationController
     end
 
     def new
-        @tv_shows = TvShow.new
+        @tv_show = TvShow.new
     end
+
+    def create
+
+    end
+
+    private
+
+        def tv_show_params
+            params.require(:tv_show).permit(:name)
+        end
 end
